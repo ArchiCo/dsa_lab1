351ee43f646c9e116093ad73a80275072171cc74
EDT4 <toad_4-edw@live.se>
2018-02-05 09:37:41 +0100

    Work

--- src/Array2.java
+++ src/Array2.java
@@ -45,6 +45,12 @@ class Array2 {
 
   // Reverse the array
   public void reverse() {
+    int tmp;
+    for (int i = 0; i < size/2; ++i) {
+      tmp = arr[i];
+      arr[i] = arr[size-1-i];
+      arr[size-1-i] = tmp;
+    }
   }
 
   // Find the maximum odd number in the array. Return -1
@@ -54,13 +60,19 @@ class Array2 {
   }
 
   // Remove the element of index i from the array.
-  public void remove(int i) {
+  public void remove(int i) { //Question 3: O(n) because one for loop
+    --size;
+
+    for (; i < size; ++i) {
+      arr[i] = arr[i+1];
+    }
   }
 
   // Remove the element of index i from the array.
   // This method may change the order of the other
   // elements of the array.
-  public void removeFast(int i) {
+  public void removeFast(int i) { //Question 3: O(1)
+    arr[i] = arr[--size];
   }
 
   // Return the index of the first occurrence of x in the array,
@@ -86,7 +98,21 @@ class Array2 {
   // assuming that the array contains a cyclic shift of
   // a non-decreasing sequence.
   public int findSplice() {
-    return 0;
+    int left   = 0;
+    int right  = size-1;
+    int middle = size/2;
+
+    while(right-left > 1){
+      if(arr[middle] <= arr[right]){
+        right = middle;
+      }else{
+        left = middle;
+      }
+
+      middle = (right-left)/2 + left;
+    }
+
+    return arr[left] <= arr[right]? left : right;
   }
 
   // Return the median value of an array.
@@ -99,5 +125,116 @@ class Array2 {
     a.set(0, 4);
     a.set(1, 2);
     System.out.println(a.toString());
+
+    Array2 a2 = new Array2(5);
+    a2.set(0,5);
+    a2.set(1,5);
+    a2.set(2,0);
+    a2.set(3,2);
+    a2.set(4,4);
+    a2.set(5,4);
+    System.out.println(a2.toString());
+    //a2.reverse();
+    //a2.removeFast(3);
+    //System.out.println(a2.findSplice());
+    System.out.println(a2.toString());
   }
 }
+
+/*
+Question 1:
+  reverse is O(n):
+    One for loop that depends on the size of the array linearly.
+    Stuff inside the loop is O(1).
+
+Question 3:
+  Assumes (i < n).
+  remove is O(n):
+    One for loop that depends on the size of the array linearly.
+    Stuff inside the loop is O(1).
+  remove2 is O(1):
+    There is only stuff that is O(1).
+
+Question 5:
+  The memory needs to be reallocated when shrinking (and possibly also moved depending on the allocator).
+  Therefore: May be inefficient when having few elements and removing.
+  Shrinking when necessary (manually) would probably be the best because the user of the dynamic array will know how it is going to be used.
+
+Question 7:
+  I assume that (+), (*), (/) is O(1). (constant^n) is O(n). (n^constant) is O(1)
+  O(n)
+  O(2*n) = O(n)
+  O(n*(n+1)/2 - 1) = O(n^2)
+  O(lg(n)*n) = O(n log n)
+  O(n)
+
+n=5
+2
++2*2
++2*2*2
++2*2*2*2
++2*2*2*2*2
+
+Question 9:
+  Algorithm 1:
+    (a,x) -> {
+      int result = 0;
+      int xTmp = 1;
+      for(int i=0; i<=length(a); ++i){
+        result += a[i]*xTmp;
+        xTmp *= x;
+      }
+      return result;
+    }
+
+    Saves the previous value of the x power.
+
+    The function is O(n):
+      One for loop that depends on the size of the array linearly.
+      Stuff inside the loop is O(1).
+
+  Algorithm 2: O(n)
+    (a,x) -> {
+      int result = 0;
+      for(int i=n-1; i>0; --i){
+        result = x*(result + a[i]);
+      }
+      return a[0] + result;
+    }
+
+    This is the formula: a0+x*(a1+x*(a2+x*(..))).
+
+    The function is O(n):
+      One for loop that depends on the size of the array linearly.
+      Stuff inside the loop is O(1).
+
+Question 11:
+  T(n) = T(n−1) + 𝛩(1/n)
+  T(n)−T(n−1) = 𝛩(1/n)
+  T(n) = 𝛩(1/n²)
+  T(n) = 𝛩(n⁻²)
+
+  T(n) = 3⋅T(n/2) + 𝛩(n²)
+  L = log₂(3) ≈ 1.585
+  c = 1.6
+  nᶜ is 𝑂(n²)
+  3⋅(n/2)² ≤ k⋅n²
+  3⋅n²/4 ≤ k⋅n²
+  (3/4)⋅n² ≤ k⋅n²
+  (3/4)⋅n² ≤ 1⋅n²
+  (3/4)⋅n² ≤ n²
+  T is 𝛩(n²)
+
+  T(n) = 4⋅T(n/2) + 𝛩(n²)
+  L = log₂(4) = 2
+  n² ⋅ log(n)⁰ is 𝛩(n²)
+  T is 𝛩(n² ⋅ log(n))
+
+  T(n) = 5⋅T(n/2) + 𝛩(n²)
+  L = log₂(5) ≈ 2.322
+  n² is 𝑂(nᴸ)
+  T is 𝛩(nᴸ) and also 𝑂(n³)
+
+Question 13:
+  Assumes a non-empty arr.
+ */
